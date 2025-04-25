9c6938779f984e8a8c365741f4ee078896c82113
Pasindu Malinda Waidyarathne <108586125+PasinduWaidyarathna@users.noreply.github.com>
2025-04-25 09:33:51 +0530

    initial commit

---
+++ src/main/java/com/example/student_managemt_system/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.example.student_managemt_system.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String testEndpoint() {
+        return "Hello, Spring Boot!";
+    }
+}
+
---
+++ src/test/java/com/example/student_managemt_system/StudentManagemtSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.student_managemt_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagemtSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
