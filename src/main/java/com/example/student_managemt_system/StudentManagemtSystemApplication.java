020a1aea02132e1808238fb42e15623e79e3e535
Pasindu Malinda Waidyarathne <108586125+PasinduWaidyarathna@users.noreply.github.com>
2025-05-06 16:30:57 +0530

    change yaml files

--- src/main/java/com/example/student_managemt_system/StudentManagemtSystemApplication.java
+++ src/main/java/com/example/student_managemt_system/StudentManagemtSystemApplication.java
@@ -1,9 +1,23 @@
 package com.example.student_managemt_system;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title="Student Management System OPEN API",
+				version = "1.0.0",
+				description="Student Management System OPEN API documentation"
+		),
+		servers = @Server(
+				url ="http://aws-public-ip:8080/api/v1",
+				description="Student Management System OPEN API url"
+		)
+)
 public class StudentManagemtSystemApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/student_managemt_system/controller/TestController.java
+++ src/main/java/com/example/student_managemt_system/controller/TestController.java
@@ -1,12 +1,8 @@
 package com.example.student_managemt_system.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*")
 @RestController
